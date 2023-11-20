d930e0b3bcf3c8b79583e1e0fcb2812b2688215c
Lucien George <luciengeorge95@gmail.com>
2023-11-20 10:23:40 +0000

    Add devise authentication to rails app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
   def home
+    # @flat = Flat.new(flat_params)
+    # if @flat.save
+    #   redirect flat_path(@flat)
+    # flash[:notice] = "Flat was successfully created."
+    # redirect_to contact_path
+    # else
+    #   render :new
+    # flash[:alert] = "Flat name can't be blank."
+    # redirect_to contact_path, flash: { alert: "Flat name can't be blank." }
+    # end
+  end
+
+  def contact
   end
 end
